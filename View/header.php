4eb43355dc10b07279431e8830b890b56d0cae00
Nilaugi <77484345+Nilaugi@users.noreply.github.com>
2022-04-27 12:36:46 +0200

    Create header.php

---
+++ View/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../View/index/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+  <title>ALL4SPORT | Article de sport</title>
+  <script src="https://kit.fontawesome.com/3ead8a56ce.js" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+
+<body>
